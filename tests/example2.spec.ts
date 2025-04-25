b17d88c426f54bfadc11c3f1751b175565ed12a6
Katie Walker <32751159+katiewalk@users.noreply.github.com>
2025-04-25 00:18:03 -0500

    refactoring

--- tests/example2.spec.ts
+++ tests/example2.spec.ts
@@ -1,46 +1,25 @@
-import { test, type Page } from '@playwright/test';
+import { test, expect, type Page } from '@playwright/test';
 import { HomePage } from '../pages/home-page';
-import { TopMenuPage } from '../pages/top-menu-page';
 
 const URL = 'https://playwright.dev/';
 let homePage: HomePage;
-let topMenuPage: TopMenuPage;
-const pageUrl = /.*intro/;
 
 test.beforeEach(async ({page}) => {
     await page.goto(URL);
     homePage = new HomePage(page);
 });
 
-async function clickGetStarted(page: Page) {
+async function clickGetStarted(page:Page) {
     await homePage.clickGetStarted();
-    topMenuPage = new TopMenuPage(page);
-}
+};
 
-test.describe('Playwright website', () => {
-
-    test('has title', async () => {
-        await homePage.assertPageTitle();
+test.describe('Playwright Website', () => {
+    test('has title', async ({ page }) => {
+        await expect(page).toHaveTitle(/Playwright/);
     });
     
     test('get started link', async ({ page }) => {
-        // Act
         await clickGetStarted(page);
-        // Assert
-        await topMenuPage.assertPageUrl(pageUrl);
+        await expect(page).toHaveURL(/.*intro/);
     });
-    
-    test('check Java page', async ({ page }) => {
-        await test.step('Act', async () => {
-            await clickGetStarted(page);
-            await topMenuPage.hoverNode();
-            await topMenuPage.clickJava();
-        });
-      
-        await test.step('Assert', async () => {
-            await topMenuPage.assertPageUrl(pageUrl);
-            await topMenuPage.assertNodeDescriptionNotVisible();
-            await topMenuPage.assertJavaDescriptionVisible();
-        });
-    });
-});
+})
