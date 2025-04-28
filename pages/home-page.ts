d1a97723b24bb0cb2f927235670597ef44e95f43
Katie Walker <32751159+katiewalk@users.noreply.github.com>
2025-04-27 19:02:41 -0500

    formatting

--- pages/home-page.ts
+++ pages/home-page.ts
@@ -1,22 +1,22 @@
-import { type Locator, type Page, expect } from '@playwright/test';
+import { type Locator, type Page, expect } from "@playwright/test";
 
-export class HomePage {
+export class HomePage{
     readonly page: Page;
     readonly getStartedButton: Locator;
-    readonly pageTitle: RegExp;
+    readonly title: RegExp;
 
-    constructor(page: Page) {
+    constructor (page: Page) {
         this.page = page;
-        this.getStartedButton = page.getByRole('link', { name: 'Get started' });
-        this.pageTitle = /Playwright/;
+        this.getStartedButton = page.getByRole('link', {name: "Get Started" })
+        this.title = /Playwright/;
     }
 
-    async clickGetStarted() {
+    async clickGetStarted (){
         await this.getStartedButton.click();
     }
 
-    async assertPageTitle() {
-        await expect(this.page).toHaveTitle(this.pageTitle);
+    async assertPageTitle(){
+        await expect(this.page).toHaveTitle(this.title);
     }
 }
 
